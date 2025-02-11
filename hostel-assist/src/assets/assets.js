cb4b24eb0c292cf5d655717f657a5dfae60264b4
mahek <mahekmodi04@gmail.com>
2025-02-11 11:57:38 +0530

    changes

--- hostel-assist/src/assets/assets.js
+++ hostel-assist/src/assets/assets.js
@@ -1,6 +1,14 @@
 import StudLogin from './StudLogin.jpg'
+import AdminLogin from './AdminLogin.jpg'
+import VitHome from './VitHome.jpg'
+import AC_Complaint from './AC_Complaint.jpg'
+
+
 
 export const assets = {
-    StudLogin
+    StudLogin,
+    AdminLogin,
+    VitHome,
+    AC_Complaint
     
 }
