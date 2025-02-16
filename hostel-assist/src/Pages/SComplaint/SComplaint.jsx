8eedcfac42d193e10c26f31866ab5d4a0ce8a127
eakshitha <eakshitha2005@gmail.com>
2025-02-16 23:34:34 +0530

    Your commit message

--- hostel-assist/src/Pages/SComplaint/SComplaint.jsx
+++ hostel-assist/src/Pages/SComplaint/SComplaint.jsx
@@ -1,52 +1,56 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./SComplaint.css"; // Importing CSS
 
 // Import a sample image for all options
 import { assets } from "../../assets/assets";
 
 const SComplaint = () => {
+  const navigate = useNavigate(); // Hook for navigation
+
   return (
     <div className="scomplaint-container">
-      <h1>Post a Complaint</h1>
-      <br/>
-      <p className="description">
-        Select a category below to report an issue. Our team will address your complaint as soon as possible.
-      </p>
-      <br/>
+      <h1 id="maintxt">Post a Complaint</h1>
+      <br />
+      <p className="description">Select a category below to report an issue</p>
+      <br />
       <div className="complaint-options">
         <Link to="/complaint-form?category=AC" className="option">
           <img src={assets.image3} alt="AC" />
-          <br/>
+          <br />
           <span>AC</span>
         </Link>
         <Link to="/complaint-form?category=Plumber" className="option">
           <img src={assets.image4} alt="Plumber" />
-          <br/>
+          <br />
           <span>Plumber</span>
         </Link>
         <Link to="/complaint-form?category=Carpenter" className="option">
           <img src={assets.image5} alt="Carpenter" />
-          <br/>
+          <br />
           <span>Carpenter</span>
         </Link>
         <Link to="/complaint-form?category=Water Cooler" className="option">
           <img src={assets.image6} alt="Water Cooler" />
-          <br/>
+          <br />
           <span>Water Cooler</span>
         </Link>
         <Link to="/complaint-form?category=Housekeeping" className="option">
           <img src={assets.image7} alt="Housekeeping" />
-          <br/>
-          <span>House keeping</span>
+          <br />
+          <span>Housekeeping</span>
         </Link>
-
         <Link to="/complaint-form?category=Electrician" className="option">
-          <img src={assets.image8} alt="Housekeeping" />
-          <br/>
-          <span>Electrician </span>
+          <img src={assets.image8} alt="Electrician" />
+          <br />
+          <span>Electrician</span>
         </Link>
       </div>
+
+      {/* Go Back Button */}
+      <button className="go-back-btn" onClick={() => navigate("/student")}>
+        Go Back
+      </button>
     </div>
   );
 };
