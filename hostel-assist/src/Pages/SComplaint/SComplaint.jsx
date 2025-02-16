4a3f929de58310558e0879bbb399661bcce947f7
eakshitha <eakshitha2005@gmail.com>
2025-02-16 23:23:55 +0530

    Your commit message

--- hostel-assist/src/Pages/SComplaint/SComplaint.jsx
+++ hostel-assist/src/Pages/SComplaint/SComplaint.jsx
@@ -1,17 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./SComplaint.css"; // Importing CSS specific to this component
+import "./SComplaint.css"; // Importing CSS
+
+// Import a sample image for all options
+import { assets } from "../../assets/assets";
 
 const SComplaint = () => {
   return (
     <div className="scomplaint-container">
       <h1>Post a Complaint</h1>
+      <br/>
+      <p className="description">
+        Select a category below to report an issue. Our team will address your complaint as soon as possible.
+      </p>
+      <br/>
       <div className="complaint-options">
-        <Link to="/complaint-form?category=AC" className="option">AC</Link>
-        <Link to="/complaint-form?category=Plumber" className="option">Plumber</Link>
-        <Link to="/complaint-form?category=Carpenter" className="option">Carpenter</Link>
-        <Link to="/complaint-form?category=Water Cooler" className="option">Water Cooler</Link>
-        <Link to="/complaint-form?category=Housekeeping" className="option">Housekeeping</Link>
+        <Link to="/complaint-form?category=AC" className="option">
+          <img src={assets.image3} alt="AC" />
+          <br/>
+          <span>AC</span>
+        </Link>
+        <Link to="/complaint-form?category=Plumber" className="option">
+          <img src={assets.image4} alt="Plumber" />
+          <br/>
+          <span>Plumber</span>
+        </Link>
+        <Link to="/complaint-form?category=Carpenter" className="option">
+          <img src={assets.image5} alt="Carpenter" />
+          <br/>
+          <span>Carpenter</span>
+        </Link>
+        <Link to="/complaint-form?category=Water Cooler" className="option">
+          <img src={assets.image6} alt="Water Cooler" />
+          <br/>
+          <span>Water Cooler</span>
+        </Link>
+        <Link to="/complaint-form?category=Housekeeping" className="option">
+          <img src={assets.image7} alt="Housekeeping" />
+          <br/>
+          <span>House keeping</span>
+        </Link>
+
+        <Link to="/complaint-form?category=Electrician" className="option">
+          <img src={assets.image8} alt="Housekeeping" />
+          <br/>
+          <span>Electrician </span>
+        </Link>
       </div>
     </div>
   );
--- hostel-assist/src/assets/assets.js
+++ hostel-assist/src/assets/assets.js
@@ -5,6 +5,12 @@ import AC_Complaint from './AC_Complaint.jpg'
 import HeaderImg from './HeaderImg.jpg'
 import image from './image.png'
 import image2 from './image2.png'
+import image3 from './image3.png'
+import image4 from './image4.png'
+import image5 from './image5.png'
+import image6 from './image6.png'
+import image7 from './image7.png'
+import image8 from './image8.png'
 export const assets = {
     StudLogin,
     AdminLogin,
@@ -12,5 +18,11 @@ export const assets = {
     AC_Complaint,
     HeaderImg,
     image,
-    image2
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8
 }
